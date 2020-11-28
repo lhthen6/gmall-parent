8cb5219e12d031d7d1c461096eb19c720863639d
lht <lht@126.com>
2020-11-28 14:09:48 +0800

    first

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,44 @@
+package com.atguigu.gmall.product.service.impl;
+
+import com.atguigu.gmall.model.product.BaseCategory1;
+import com.atguigu.gmall.model.product.BaseCategory2;
+import com.atguigu.gmall.model.product.BaseCategory3;
+import com.atguigu.gmall.product.mapper.BaseCategory1Mapper;
+import com.atguigu.gmall.product.mapper.BaseCategory2Mapper;
+import com.atguigu.gmall.product.mapper.BaseCategory3Mapper;
+import com.atguigu.gmall.product.service.CategoryService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private BaseCategory1Mapper baseCategory1Mapper;
+    @Autowired
+    private BaseCategory2Mapper baseCategory2Mapper;
+    @Autowired
+    private BaseCategory3Mapper baseCategory3Mapper;
+
+    @Override
+    public List<BaseCategory1> getCategory1() {
+        return baseCategory1Mapper.selectList(null);
+    }
+
+    @Override
+    public List<BaseCategory2> getCategory2(Long category1Id) {
+        QueryWrapper<BaseCategory2> wrapper = new QueryWrapper<>();
+        wrapper.eq("category1_id", category1Id);
+        return baseCategory2Mapper.selectList(wrapper);
+    }
+
+    @Override
+    public List<BaseCategory3> getCategory3(Long category2Id) {
+        QueryWrapper<BaseCategory3> wrapper = new QueryWrapper<>();
+        wrapper.eq("category2_id", category2Id);
+        return baseCategory3Mapper.selectList(wrapper);
+    }
+}
